c963ecac50d3f6931f956af7a0ec1598ea06114f
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-03 10:54:41 +0100

    Test driven post class

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,1 +1,19 @@
 from lib.post import Post
+
+def test_initialised_properly():
+    post = Post(1, 'My first post', 'Hello world!', 0, 1)
+
+    assert post.id == 1
+    assert post.title == 'My first post'
+    assert post.content == 'Hello world!'
+    assert post.views == 0
+    assert post.account_id == 1
+
+def test_repr():
+    post = Post(1, 'My first post', 'Hello world!', 0, 1)
+    assert str(post) == "Post(1, My first post, Hello world!, 0, 1)"
+
+def test_eq():
+    post_1 = Post(1, 'My first post', 'Hello world!', 0, 1)
+    post_2 = Post(1, 'My first post', 'Hello world!', 0, 1)
+    assert post_1 == post_2
