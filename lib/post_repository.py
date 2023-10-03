a5c99601662a2650eceae2f132e54c1dd6331fdc
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-03 11:22:03 +0100

    Test driven post repository

--- lib/post_repository.py
+++ lib/post_repository.py
@@ -1,4 +1,32 @@
 from lib.post import *
 
 class PostRepository():
-    pass
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM posts')
+        posts = []
+
+        for row in rows:
+            posts.append(Post(row['id'], row['title'], row['content'], row['views'], row['account_id']))
+
+        return posts
+
+    def find(self, id):
+        row = self._connection.execute(f"SELECT * FROM posts WHERE id = {id}")[0]
+
+        return Post(row['id'], row['title'], row['content'], row['views'], row['account_id'])
+
+    def find_account_posts(self, account_id):
+        rows = self._connection.execute(f"SELECT * FROM posts WHERE account_id = {account_id}")
+        posts = []
+
+        for row in rows:
+            posts.append(Post(row['id'], row['title'], row['content'], row['views'], row['account_id']))
+
+        return posts
+
+    def create_post(self, title, content, account_id):
+        query = f"INSERT INTO posts (title, content, views, account_id) VALUES ('{title}', '{content}', 0, {account_id})"
+        self._connection.execute(query)
--- tests/test_post_repository.py
+++ tests/test_post_repository.py
@@ -1,2 +1,37 @@
 from lib.post_repository import *
 
+def test_all_posts(db_connection):
+    db_connection.seed('seeds/social_network.sql')
+    post_repo = PostRepository(db_connection)
+
+    posts = post_repo.all()
+
+    assert len(posts) == 3
+
+def test_find_post(db_connection):
+    db_connection.seed('seeds/social_network.sql')
+    post_repo = PostRepository(db_connection)
+
+    post = post_repo.find(1)
+
+    assert post.id == 1
+    assert post.title == 'My first post'
+    assert post.content == 'This is my first post'
+
+def test_find_account_posts(db_connection):
+    db_connection.seed('seeds/social_network.sql')
+    post_repo = PostRepository(db_connection)
+
+    posts = post_repo.find_account_posts(1)
+
+    assert len(posts) == 2
+    assert posts[0].title == 'My first post'
+    assert posts[1].title == 'My second post'
+
+def test_create_post(db_connection):
+    db_connection.seed('seeds/social_network.sql')
+    post_repo = PostRepository(db_connection)
+
+    post_repo.create_post('My third post', 'Hello third world!', 1)
+
+    assert len(post_repo.all()) == 4
